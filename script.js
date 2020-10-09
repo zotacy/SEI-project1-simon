d0706d6edb35b1df0ad4c137537d1a160b380d51
zotacy <tacyzacharyo@johndeere.com>
2020-10-09 14:34:55 -0400

    Removed RESET button, dual functionality in 'start new game' button. resets moves to empty and initalizes new simon sequence

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let sequence = [];
 let moves = [];
 
 // add event listeners to each button
-cellArray.forEach(cell => cell.addEventListener('click', ()=>{console.log(`clicked`)}));
+cellArray.forEach(cell => cell.addEventListener('click', clickMoves));
 
 // generate random sequence
     //queue color with brighter color.
@@ -22,12 +22,20 @@ function makeSequence(){
 // add sequence tracking
     // when button clicked, add to sequence.
 function clickMoves(){
-
+    moves.push(this.id)
+    console.log(this.id)
+    console.log(moves)
 }
 // add losing logic: wrong button clicked in sequence
     //New array with clicks
     //compare to sequence array
     // iif they don't match, end game/ prevent clicking further.
 
+// Start Button-  Sets Move array to empty and initalizes makeSequence
+const startBtn = document.querySelector('#start')
+startBtn.addEventListener('click', ()=>{
+    moves=[];
+    makeSequence;
+})
 
 
