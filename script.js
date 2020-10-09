60c0ec0020c9c6c8861a65a76db484def53e101e
zotacy <tacyzacharyo@johndeere.com>
2020-10-09 12:33:04 -0400

    basic js wireframe/ logic setup

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-// Variables- 4 divs with colors
+// Variables-
+const gameBoard = document.querySelector('.gameBoard');
+const cellArray = document.querySelectorAll('.cell');
+
+const red = document.querySelector('#red');
+const yellow = document.querySelector('#yellow');
+const blue = document.querySelector('#blue');
+const green = document.querySelector('#green');
+
+let counter = 0;
+let sequence = [];
+let moves = [];
 
 // add event listeners to each button
+cellArray.forEach(cell => cell.addEventListener('click', ()=>{console.log(`clicked ${gameBoard.cell}`)}));
 
 // generate random sequence
-    //queue color with brighter color (sound for silver)
+    //queue color with brighter color.
+function makeSequence(){
+
+}
 // add sequence tracking
     // when button clicked, add to sequence.
+function clickMoves(){
 
+}
 // add losing logic: wrong button clicked in sequence
     //New array with clicks
     //compare to sequence array
