b419115a749022ee6aa5e4a312cda9ae327e982f
zotacy <tacyzacharyo@johndeere.com>
2020-10-12 14:30:28 -0400

    Updated timing

--- script.js
+++ script.js
@@ -20,16 +20,18 @@ function illuminate(current){
     current.style.opacity = '0.5';
     setTimeout(()=>{
         resetColor(current)
-    }, 500);
+    }, 300);
 };
 function displayPlaySequence(){
+    cellArray.forEach(cell => cell.removeEventListener('click', clickMoves)); //Removes button clicking
     playSequence.forEach((color, index) => {
         let currentDiv = document.querySelector(`#${color}`)
-        console.log(currentDiv)
+        //console.log(currentDiv)
         setTimeout(() => {
             illuminate(currentDiv)
-        }, 1500*index+1);
+        }, 800*index+1);
     });
+    cellArray.forEach(cell => cell.addEventListener('click', clickMoves)); //Add clicking function back
 };
 //--//
 
@@ -44,7 +46,7 @@ function makeSequence(){
                 let cellId= cellArray[Math.floor(Math.random()*cellArray.length)].id;
                 sequence.push(cellId)
         };
-        console.log(sequence);
+        // console.log(sequence);
         playSequence.push(sequence[counter]);
         displayPlaySequence();
         // console.log(playSequence)
@@ -52,32 +54,28 @@ function makeSequence(){
 };
 function updateSequence(){
     playSequence.push(sequence[counter])
+    // console.log(playSequence)
     displayPlaySequence();
-        // Add level based on counter.
-        moves = [];
-        counter = 0;
-            console.log(playSequence)
+    moves = [];
+    counter = 0;  // Add level based on counter.
 };
 
 // Move tracking
     // when button clicked, add move to move array.
     // If moves are correct, initiate updateSequence.
-    // If wrong move, end game.
 function clickMoves(){
     moves.push(this.id); //adds move to moves array
-    //this.style.opacity= '0.5'
     illuminate(this);
     counter ++;
     for (let i=0; i<moves.length; i++){
-        if (moves[i] !== playSequence[i]){ //Lose logic
+        if (moves[i] !== playSequence[i]){ //Lose logic- END GAME
             alert (`You Lose. Press OK to start a new game.`);
             moves = [];
             playSequence = [];
-            // // cellArray.forEach(cell => cell.removeEventListener('click', clickMoves)); //Removes button clicking
         };
     };
     if (moves.length === playSequence.length){
-        setTimeout(updateSequence, 1000)
+        setTimeout(updateSequence, 800)
     };
 };
 
